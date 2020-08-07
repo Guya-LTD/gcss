8b79c004b0275581368e5f6a69acc8bf6e911b77
Simonbelete <simonbelete@gmail.com>
2020-08-07 03:26:23 +0300

    feat(navbar): navbar and icon

--- js/src/navbar.js
+++ js/src/navbar.js
@@ -1,14 +1,18 @@
 //style({display: 'block'})
 window.openNavbar = function openNavbar(navBtn) {    
     var navbar = navBtn.parentElement
-    var navbarMenu = document.getElementById('navbar-menu')
+    navbar = navbar.parentElement
+    var navbarBrand = navbar.parentNode.getElementsByClassName('navbar__brand')[0];
+    console.log(navbarBrand)
     navbar.classList.add('navbar--show')
+    navbarBrand.classList.add('navbar__brand--remove')
     //navbarMenu.classList.add('navbar--show')
 }
 
 
 window.closeNavbar = function closeNavbar(navBtn) {    
     var navbar = navBtn.parentElement
-    var navbarMenu = document.getElementById('navbar-menu')
+    var navbarBrand = navbar.parentNode.getElementsByClassName('navbar__brand')[0];
     navbar.classList.remove('navbar--show')
+    navbarBrand.classList.remove('navbar__brand--remove')
 }
