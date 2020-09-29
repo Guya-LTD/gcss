b7ee2e88f4bd9da33312a95f41f2ed843d84ec10
Simonbelete <simonbelete@gmail.com>
2020-09-29 20:13:11 +0300

    feat(navbar): adding open and close support to navbar

--- js/src/navbar.js
+++ js/src/navbar.js
@@ -1,18 +1,32 @@
 //style({display: 'block'})
 window.openNavbar = function openNavbar(navBtn) {    
-    var navbar = navBtn.parentElement
+    /*var navbar = navBtn.parentElement
     navbar = navbar.parentElement
     var navbarBrand = navbar.parentNode.getElementsByClassName('navbar__brand')[0];
-    console.log(navbarBrand)
+    console.log(navbarBrand)*/
+
+    var navbar = document.getElementById('navbar')
     navbar.classList.add('navbar--show')
-    navbarBrand.classList.add('navbar__brand--remove')
+    //navbarBrand.classList.add('navbar__brand--remove')
     //navbarMenu.classList.add('navbar--show')
+
+    /*navbarClose = document.getElementById('navbar-close')
+    navCollapse = document.getElementById('navbar-collapse')
+    navCollapse.classList.add('navbar--show')
+    navbarClose.classList.add('navbar--show')*/
 }
 
 
 window.closeNavbar = function closeNavbar(navBtn) {    
-    var navbar = navBtn.parentElement
-    var navbarBrand = navbar.parentNode.getElementsByClassName('navbar__brand')[0];
+    /*var navbar = navBtn.parentElement
+    var navbarBrand = navbar.parentNode.getElementsByClassName('navbar__brand')[0];*/
+
+    var navbar = document.getElementById('navbar')
     navbar.classList.remove('navbar--show')
     navbarBrand.classList.remove('navbar__brand--remove')
+
+    /*navbarClose = document.getElementById('navbar-close')
+    navCollapse = document.getElementById('navbar-collapse')
+    navCollapse.classList.remove('navbar--show')
+    navbarClose.classList.remove('navbar--show')*/
 }
